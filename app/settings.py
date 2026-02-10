464355131a4cc50eb9ad44dea7116c3df67ace14
BillyHK123 <bellykwan@gmail.com>
2026-02-11 02:38:08 +0800

    revise_osget2

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 class Settings(BaseModel):
-    poe_api_key: str = os.environ["POE_API_KEY"]
+    poe_api_key: str = os.getenv("POE_API_KEY", "")
     poe_bot_name: str = os.getenv("POE_BOT_NAME", "GPT-3.5-Turbo")
 
     embedding_model: str = os.getenv(
